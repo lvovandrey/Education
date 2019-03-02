d3e973ac1f1d741f2ecd1030c82b59b187d9e736
lvovandrey <lvovandrey@mail.ru>
2019-03-02 12:55:49 +0300

    Create class HelloClass

---
+++ WpfGitEdu/WpfGitEdu/HelloClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfGitEdu
+{
+    public class HelloClass
+    {
+        public void Hello()
+        {
+            MessageBox.Show("Hello world");
+        }
+    }
+}
